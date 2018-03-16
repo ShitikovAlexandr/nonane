61d95ac0335e23b3aaf6ef461fa1b518b3de042f
Alexandr Shitikov <shitikov.net@gmail.com>
2018-03-16 18:21:28 +0200

    add pod project

---
+++ BestToBiz/Managers/BBNetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  BBNetworkManager.swift
+//  BestToBiz
+//
+//  Created by Gemicle Inc on 3/16/18.
+//  Copyright © 2018 Gemicle. All rights reserved.
+//
+
+import Foundation
+
+class BBNetworkManager: NSObject {
+
+}
---
+++ Pods/Target Support Files/Pods-BestToBiz/Pods-BestToBiz-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestToBiz : NSObject
+@end
+@implementation PodsDummy_Pods_BestToBiz
+@end
